8f42420415b5d32bc03efec32cff9281d59ababe
jihun04 <yujihun20060217@gmail.com>
2021-06-13 21:31:05 +0900

    Gulp is Awesome

--- src/server.js
+++ src/server.js
@@ -19,5 +19,10 @@ const server = app.listen(PORT, handleListening);
 const io = socketIO(server);
 
 io.on("connection", socket => {
-  socket.on("something", () => console.log("something"))
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", { message, nickname: socket.nickname || "Anon" })
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
 });
